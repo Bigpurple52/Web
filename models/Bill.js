2cce1543749b7e4a2d261d5ff87e8cd5a54d9172
Bigpurple52 <finalfantasy_cristalchronicle@hotmail.com>
2015-12-11 12:10:44 +0100

    Correctif de la BDD

--- models/Bill.js
+++ models/Bill.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var BillSchema = new mongoose.Schema({
   userpaid: String,
   users: [],
-  date: {type: String, default: date()}
+  date: {type: String, default: Date()}
 });
 
 mongoose.model('Bill', BillSchema);
--- models/Payment.js
+++ models/Payment.js
@@ -4,7 +4,7 @@ var PaymentSchema = new mongoose.Schema({
   userpaid: String,
   usertake: String,
   amount: {type: Number, default: 0},
-  date: {type: String, default: date()}
+  date: {type: String, default: Date()}
 });
 
 mongoose.model('Payment', PaymentSchema);
