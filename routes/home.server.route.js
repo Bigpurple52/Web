6e024065c5c510058d2399d02b8068ecd78e4bc7
Thomasgitt <thomas.gittinger@live.fr>
2016-01-21 15:02:00 +0100

    Fichiers oubliés d'Olivier

--- routes/home.server.route.js
+++ routes/home.server.route.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-var groups = mongoose.model('Group');
+var DBgroups = mongoose.model('Group');
 
 router.get('/home', function(req, res) {
-	 groups.find(function(err, doc) {
+	 DBgroups.find(function(err, doc) {
         if (err) {
             return (err);
         }
@@ -12,4 +12,18 @@ router.get('/home', function(req, res) {
     });
 });
 
+router.get('/home/friends/:mail', function(req, res) {
+	DBgroups.find({"users.mail": req.params.mail, "type": "FRIEND"},function(err,data){
+		if (err) { return (err); }
+			res.json(data);
+	});
+});
+
+
+router.get('/home/groups/:mail', function(req, res) {
+	DBgroups.find({"users.mail": req.params.mail, "type": "GROUP"},function(err,data){
+		if (err) { return (err); }
+			res.json(data);
+	});
+});
 module.exports = router;
